21eb3aa07f30c2f1999dfb190bf2060f66755587
Roytman Piccoli <roytmanpiccoli@macbook-air-de-roytman.home>
2021-05-06 11:46:21 +0200

    first commit

---
+++ webIdentityFederation/originalWebFeb.py
@@ -0,0 +1,17 @@
+import boto3
+
+client = boto3.client('sts')
+
+arn = 'arn:aws:iam::xxxxxxxxxxxx:role/WebIdFed_Amazon'
+session_name = 'web-identity-federation'
+token = '...'
+
+creds = client.assume_role_with_web_identity(
+    RoleArn=arn,
+    RoleSessionName=session_name,
+    WebIdentityToken=token,
+    ProviderId='www.amazon.com',
+)
+
+print creds['AssumedRoleUser']['Arn']
+print creds['AssumedRoleUser']['AssumedRoleId']
